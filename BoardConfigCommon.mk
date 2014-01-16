2eada9cb3223fd8e0b0c2353f35ac4a3629c4795
Flemmard <flemmard@gmail.com>
2014-01-16 16:55:26 +0100

    SELinux: add sepolicy from 8960
    Change-Id: I044258012e77d901462c1ba3cf08c0441f0c2799

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,6 +83,32 @@ TARGET_DISPLAY_INSECURE_MM_HEAP := true
 TARGET_PROVIDES_LIBLIGHT := true
 TARGET_PROVIDES_LIBLIGHTS := true
 
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/htc/msm8660-common/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    app.te \
+    bluetooth.te \
+    device.te \
+    domain.te \
+    drmserver.te \
+    file_contexts \
+    file.te \
+    hci_init.te \
+    healthd.te \
+    init_shell.te \
+    init.te \
+    keystore.te \
+    kickstart.te \
+    mediaserver.te \
+    rild.te \
+    surfaceflinger.te \
+    system.te \
+    ueventd.te \
+    wpa_socket.te \
+    wpa.te
+
 # Webkit
 ENABLE_WEBGL := true
 TARGET_FORCE_CPU_UPLOAD := true
